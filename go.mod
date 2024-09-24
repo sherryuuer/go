c0b3528cafad25ce90a6a6c1187bedf42faaa213
sherryuuer <sherryuuer@gmail.com>
2024-09-24 23:36:20 +0900

    20240924

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/sherryuuer/go
-
-go 1.23.1
--- src/lectures/exercise/embedding/embedding.go
+++ src/lectures/exercise/embedding/embedding.go
@@ -12,9 +12,7 @@
 
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type Bytes int
 type Celcius float32
@@ -23,16 +21,56 @@ type BandwidthUsage struct {
 	amount []Bytes
 }
 
+func (b *BandwidthUsage) AverageBandwidth() int {
+	sum := 0
+	for i := 0; i < len(b.amount); i++ {
+		sum += int(b.amount[i])
+	}
+	return sum / len(b.amount)
+}
+
 type CpuTemp struct {
 	temp []Celcius
+}
+
+func (c *CpuTemp) AverageCpuTemp() int {
+	sum := 0
+	for i := 0; i < len(c.temp); i++ {
+		sum += int(c.temp[i])
+	}
+	return sum / len(c.temp)
 }
 
 type MemoryUsage struct {
 	amount []Bytes
 }
 
+func (m *MemoryUsage) AverageMemoryUsage() int {
+	sum := 0
+	for i := 0; i < len(m.amount); i++ {
+		sum += int(m.amount[i])
+	}
+	return sum / len(m.amount)
+}
+
+type Dashboard struct {
+	BandwidthUsage
+	CpuTemp
+	MemoryUsage
+}
+
 func main() {
 	bandwidth := BandwidthUsage{[]Bytes{50000, 100000, 130000, 80000, 90000}}
 	temp := CpuTemp{[]Celcius{50, 51, 53, 51, 52}}
 	memory := MemoryUsage{[]Bytes{800000, 800000, 810000, 820000, 800000}}
+
+	dash := Dashboard{
+		BandwidthUsage: bandwidth,
+		CpuTemp:        temp,
+		MemoryUsage:    memory,
+	}
+
+	fmt.Printf("Average Bandwidth Usage: %v\n", dash.AverageBandwidth())
+	fmt.Printf("Average Cpu temp: %v\n", dash.AverageCpuTemp())
+	fmt.Printf("Average Memory Usage: %v\n", dash.AverageMemoryUsage())
 }
